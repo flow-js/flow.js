97bcef6fb36fcaf9b1aea527c44fa905012bcdff
flow-js <89139346+flow-js@users.noreply.github.com>
2021-08-18 21:55:16 +0700

    Update parser.js

--- flow/functions/parser.js
+++ flow/functions/parser.js
@@ -1,3 +1,9 @@
 const Parser = {
-  $setServerVar: 'Set value of a server Variable;$setServerVar[variable name;variable value;guildID/serverID (optional)]'
+  $setServerVar: 'Set value of a server Variable;$setServerVar[variable name;variable value;guildID/serverID (optional)]',
+  $getServerVar: 'Get value of a server Variable;$getServerVar[variable name;guildID/serverID (optional)]',
+  $setUserVar: 'Set value of a user Variable;$setUserVar[variable name;variable value;userID (optional)]',
+  $getUserVar: 'Get value of a user Variable;$getUserVar[variable name;userID (optional)]',
+  $setGlobalUserVar: 'Set value of a global user Variable;$setGlobalUserVar[variable name;variable value;userID (optional)]',
+  $getGlobalUserVar: 'Get value of a global user Variable;$getGlobUserVar[variable name;userID (optional)]',
+  $message: 'Return what the authoe said;$message[argument number (optional)]'
 }
