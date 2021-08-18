92c44b214a23c3ab1c4093d3c792c2af50412621
flow-js <89139346+flow-js@users.noreply.github.com>
2021-08-18 22:55:10 +0700

    Create message.js

---
+++ flow/functions/funcs/message.js
@@ -0,0 +1,22 @@
+const message = cmd => {
+  let code = cmd.command.code
+  const inside = cmd.unpack()
+
+  if(inside.inside) {
+    let arguments = inside.inside
+    if (["last", ">"].includes(arguments.addBrackets()))
+       arguments = cmd.args.length
+    if (isNaN(arguments)) {
+       return cmd.error(`Function \`(${cmd.func})\` Error: \`Invalid number in ${inside}\` `)
+
+       code = code.replaceLast(`$message${inside}`, cmd.args[Number(arguments) - 1] ? cmd.args[Number(arguments) - 1].deleteBrackets() : '')  
+  } else {
+       code = code.replaceLast('$message', cmd.args.join(' ').deleteBrackets())
+  }
+
+    return {
+       code: code
+  }
+}
+
+module.exports = message;
