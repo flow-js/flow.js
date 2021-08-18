b4d4433fac957f827a018fab9761eee8626ef83d
flow-js <89139346+flow-js@users.noreply.github.com>
2021-08-18 22:57:51 +0700

    Update getGlobalUserVar.js

--- flow/functions/funcs/getGlobalUserVar.js
+++ flow/functions/funcs/getGlobalUserVar.js
@@ -1,17 +1,17 @@
-module.exports = async func => {
-  const code = func.command.code
-  const r = code.split('$getGlobalUserVar').length -1
-  const inside = code.split('$getGlobalUserVar')[r].after()
-  const error = func.inside(inside)
+module.exports = async cmd => {
+  const code = cmd.command.code
+  const val = code.split('$getGlobalUserVar').length -1
+  const inside = code.split('$getGlobalUserVar')[val].after()
+  const error = cmd.inside(inside)
   
   if(error)
     return func.error(error)
-    const [ variable, user = func.message.author.id ] = inside.splits
+    const [ variable, user = cmd.message.author.id ] = inside.splits
   if(func.client.variables[variable] === undefined)
-    return func.error(`Error: \`Variable (${variable}) not found!\` `)
-    let item = await func.client.db.get('main', `${variable}_${user}`)
+    return cmd.error(`Error: \`Variable (${variable}) not found!\` `)
+    let item = await cmd.client.db.get('main', `${variable}.${user}`)
    if(!item)
-     item = { value: func.client.variables[variable] }
+     item = { value: cmd.client.variables[variable] }
    return {
      code: code.replaceLast(`$getGlobalUserVar${inside}`, item.value)
   }
