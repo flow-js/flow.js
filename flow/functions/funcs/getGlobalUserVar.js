98d2adb13f01e67cb4696aa6f9d83788b4c71ad2
flow-js <89139346+flow-js@users.noreply.github.com>
2021-08-18 23:24:55 +0700

    Update getGlobalUserVar.js

--- flow/functions/funcs/getGlobalUserVar.js
+++ flow/functions/funcs/getGlobalUserVar.js
@@ -3,7 +3,6 @@ module.exports = async cmd => {
   const s = code.split('$getGlobalUserVar').length -1
   const inside = code.split('$getGlobalUserVar')[s].after()
   const err = cmd.inside(inside)
-  
   if(error)
     return cmd.error(err)
     const [ data, user = cmd.message.author.id ] = inside.splits
