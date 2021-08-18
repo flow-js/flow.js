795706133e51c40544f4304d5fa0eda9531e1d35
flow-js <89139346+flow-js@users.noreply.github.com>
2021-08-18 23:38:44 +0700

    Update parser.js

--- flow/functions/parser.js
+++ flow/functions/parser.js
@@ -1,9 +1,17 @@
 const Parser = {
+  // VARIABLE
   $setServerVar: 'Set value of a server Variable;$setServerVar[variable name;variable value;guildID/serverID (optional)]',
   $getServerVar: 'Get value of a server Variable;$getServerVar[variable name;guildID/serverID (optional)]',
   $setUserVar: 'Set value of a user Variable;$setUserVar[variable name;variable value;userID (optional)]',
   $getUserVar: 'Get value of a user Variable;$getUserVar[variable name;userID (optional)]',
   $setGlobalUserVar: 'Set value of a global user Variable;$setGlobalUserVar[variable name;variable value;userID (optional)]',
   $getGlobalUserVar: 'Get value of a global user Variable;$getGlobUserVar[variable name;userID (optional)]',
-  $message: 'Return what the authoe said;$message[argument number (optional)]'
+  // EMBED
+  $description: 'Set embed Description;$description[text]",
+  $title: 'Set embed Title;$title[text;link]',
+  // GENERAL
+  $message: 'Return what the author said;$message[argument number (optional)]',
+  $ping: 'Return database current ping;$pingc,
+  $eval: 'Evaluating code, useable for testing;$eval[code;return code (yes/no)]',
+  
 }
