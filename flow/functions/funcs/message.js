b933f200e50564d86127053000c9069ddd74425c
flow-js <89139346+flow-js@users.noreply.github.com>
2021-08-19 11:59:35 +0700

    Update message.js

--- flow/functions/funcs/message.js
+++ flow/functions/funcs/message.js
@@ -6,7 +6,7 @@ module.exports = cmd => {
     if (["last", ">"].includes(arguments.addBrackets()))
        arguments = cmd.args.length
     if (isNaN(arguments)) {
-       return cmd.error(`Function \`(${cmd.func})\` Error: \`Invalid number in ${inside}\` `)
+       return cmd.error(`**Error:** (\`(${cmd.func})\`) \`Invalid number in ${inside}\` `)
        code = code.replaceLast(`$message${inside}`, cmd.args[Number(arguments) - 1] ? cmd.args[Number(arguments) - 1].deleteBrackets() : '')  
   } else {
        code = code.replaceLast('$message', cmd.args.join(' ').deleteBrackets())
