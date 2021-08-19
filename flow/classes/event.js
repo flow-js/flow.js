389fe0d2cfa926a00682190e9b93adfe250ebfbd
flow-js <89139346+flow-js@users.noreply.github.com>
2021-08-19 20:28:07 +0700

    Update event.js

--- flow/classes/event.js
+++ flow/classes/event.js
@@ -3,29 +3,23 @@ class CreateEvent {
   constructor(client, EventEmitter) {
     this.client = client;
     this.event = EventEmitter;
-    if (this.event._eventsCount === undefined)
+    if (this.event._eventsCount === blank)
       throw new Error('Data is not an EventEmitter');
     this.commands = [];
   }
-
   command(...args) {
     for (const d of args) {
       if (!d.name) throw new TypeError(`Event need a name!`);
-
       if (isNaN(parseInt(d.channel)))
         throw new TypeError("Event needs a Snowflake Channel ID");
-
       if (!d.listen)
         throw new TypeError(
           "Event Command needs a 'listen' to listen to an event"
         );
-
       if (!d.code) throw new TypeError(`Event Command can't be blank!`);
-
       this.commands.push(d);
     }
   }
-
   listen(name) {
     if (!name) throw new TypeError("Listener needs a Name to listen");
     const commands = this.commands.filter((f) => f.listen === name);
@@ -36,5 +30,4 @@ class CreateEvent {
     });
   }
 }
-
 module.exports = CreateEvent;
